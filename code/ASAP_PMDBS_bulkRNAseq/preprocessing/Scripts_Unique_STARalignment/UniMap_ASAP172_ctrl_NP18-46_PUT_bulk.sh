91ead916483b9f9cc7e6dd5f184ea9fbdaf1456e
raquelgarza <raquelgarza95@gmail.com>
2025-10-22 11:28:25 +0200

    feat: add mapping of deep bulk RNAseq of postmortem samples

---
+++ code/ASAP_PMDBS_bulkRNAseq/preprocessing/Scripts_Unique_STARalignment/UniMap_ASAP172_ctrl_NP18-46_PUT_bulk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -n 10
+#SBATCH -N 1
+#SBATCH -A csens2024-3-2
+#SBATCH -t 10:00:00
+#SBATCH -J ASAP172_ctrl_NP18-46_PUT_bulk.u.STAR
+#SBATCH -o ASAP172_ctrl_NP18-46_PUT_bulk.u.STAR.out
+#SBATCH -e ASAP172_ctrl_NP18-46_PUT_bulk.u.STAR.err
+ml purge
+ml GCC/10.2.0 STAR/2.7.8a
+STAR --genomeDir /scale/gr01/shared/jjakobsson/backup/raquelgg_fs5_jakobssonlab/GRCh38.p13_gencode.v38_STAR  --readFilesIn /home/yogitas/ASAP_Analysis/FastQFilesCat_ASAP_Seq319/ASAP172_ctrl_NP18-46_PUT_bulk_R1.fastq.gz /home/yogitas/ASAP_Analysis/FastQFilesCat_ASAP_Seq319/ASAP172_ctrl_NP18-46_PUT_bulk_R2.fastq.gz  --readFilesCommand gunzip -c  --runThreadN 20  --limitBAMsortRAM 7963119355  --outFilterMultimapNmax 1 --outFilterMismatchNoverLmax 0.03  --outSAMattributes All  --outSAMtype BAM SortedByCoordinate  --sjdbGTFfile /home/yogitas/fs3_files/AnnotationFiles/GRCh38/gencode.v38.annotation.gtf  --outFileNamePrefix /home/yogitas/ASAP_Analysis/Unique_hg38_STAR/ASAP172_ctrl_NP18-46_PUT_bulk
---
+++ code/ASAP_PMDBS_bulkRNAseq/preprocessing/Scripts_Unique_STARalignment/UniMap_ASAP209_PD_NP17-232_SN_bulk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#SBATCH -n 10
+#SBATCH -N 1
+#SBATCH -A csens2024-3-2
+#SBATCH -t 10:00:00
+#SBATCH -J ASAP209_PD_NP17-232_SN_bulk.u.STAR
+#SBATCH -o ASAP209_PD_NP17-232_SN_bulk.u.STAR.out
+#SBATCH -e ASAP209_PD_NP17-232_SN_bulk.u.STAR.err
+ml purge
+ml GCC/10.2.0 STAR/2.7.8a
+STAR --genomeDir /scale/gr01/shared/jjakobsson/backup/raquelgg_fs5_jakobssonlab/GRCh38.p13_gencode.v38_STAR  --readFilesIn /home/yogitas/ASAP_Analysis/FastQFilesCat_ASAP_Seq319/ASAP209_PD_NP17-232_SN_bulk_R1.fastq.gz /home/yogitas/ASAP_Analysis/FastQFilesCat_ASAP_Seq319/ASAP209_PD_NP17-232_SN_bulk_R2.fastq.gz  --readFilesCommand gunzip -c  --runThreadN 20  --limitBAMsortRAM 7963119355  --outFilterMultimapNmax 1 --outFilterMismatchNoverLmax 0.03  --outSAMattributes All  --outSAMtype BAM SortedByCoordinate  --sjdbGTFfile /home/yogitas/fs3_files/AnnotationFiles/GRCh38/gencode.v38.annotation.gtf  --outFileNamePrefix /home/yogitas/ASAP_Analysis/Unique_hg38_STAR/ASAP209_PD_NP17-232_SN_bulk
