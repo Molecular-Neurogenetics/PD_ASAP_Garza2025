177602c33172254761978f93151a5cb787691712
raquelgarza <raquelgarza95@gmail.com>
2025-10-22 11:28:53 +0200

    feat: add indexing of BAM deep bulk RNAseq of postmortem samples

---
+++ code/ASAP_PMDBS_bulkRNAseq/preprocessing/Scripts_indexBAMFiles/index_ASAP162_ctrl_NP18-148_PFC_bulkAligned.sortedByCoord.out.bam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -n 10
+#SBATCH -N 1
+#SBATCH -A csens2024-3-2
+#SBATCH -t 10:00:00
+#SBATCH -J index_ASAP162_ctrl_NP18-148_PFC_bulkAligned.sortedByCoord.out.bam
+#SBATCH -o index_ASAP162_ctrl_NP18-148_PFC_bulkAligned.sortedByCoord.out.bam.out
+#SBATCH -e index_ASAP162_ctrl_NP18-148_PFC_bulkAligned.sortedByCoord.out.bam.err
+# Generate index for bam files
+cd /home/yogitas/ASAP_Analysis/Unique_hg38_STAR
+module purge
+ml GCC/12.3.0 SAMtools/1.18
+samtools index -b ASAP162_ctrl_NP18-148_PFC_bulkAligned.sortedByCoord.out.bam
+
---
+++ code/ASAP_PMDBS_bulkRNAseq/preprocessing/Scripts_indexBAMFiles/index_ASAP173_PD_NP21-57_PUT_bulkAligned.sortedByCoord.out.bam.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -n 10
+#SBATCH -N 1
+#SBATCH -A csens2024-3-2
+#SBATCH -t 10:00:00
+#SBATCH -J index_ASAP173_PD_NP21-57_PUT_bulkAligned.sortedByCoord.out.bam
+#SBATCH -o index_ASAP173_PD_NP21-57_PUT_bulkAligned.sortedByCoord.out.bam.out
+#SBATCH -e index_ASAP173_PD_NP21-57_PUT_bulkAligned.sortedByCoord.out.bam.err
+# Generate index for bam files
+cd /home/yogitas/ASAP_Analysis/Unique_hg38_STAR
+module purge
+ml GCC/12.3.0 SAMtools/1.18
+samtools index -b ASAP173_PD_NP21-57_PUT_bulkAligned.sortedByCoord.out.bam
+
